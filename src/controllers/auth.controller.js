a4869a3492ff4133489d5b2e6b794afe82a44348
Javier Gonzalez <javgon2306@gmail.com>
2024-10-26 10:34:05 -0300

    add authController

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,14 @@
+import User from '../models/user.model.js';
+
 export const register = async (req, res) => {
     const {email, password, username} = req.body;
+
+    const newUser = new User ({
+        username,
+        email,
+        password
+
+    })
 }
 
 export const login = async (req, res) => {
