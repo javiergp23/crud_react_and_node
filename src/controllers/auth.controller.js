b642192ac1d8adc2b8358f0679974a8eed6c056a
Javier Gonzalez <javgon2306@gmail.com>
2024-10-26 10:39:01 -0300

    add newUser authController

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,13 +2,19 @@ import User from '../models/user.model.js';
 
 export const register = async (req, res) => {
     const {email, password, username} = req.body;
-
-    const newUser = new User ({
-        username,
-        email,
-        password
-
-    })
+    try{
+        const newUser = new User ({
+            username,
+            email,
+            password
+    
+        })
+        await newUser.save();
+    }catch(error){
+        console.log(error)
+    }
+    
+    res.send('registrando');
 }
 
 export const login = async (req, res) => {
